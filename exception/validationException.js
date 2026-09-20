ddac83bf145947127b800e844cfda45a464a19cb
CharanUggala <charanuggala@gmail.com>
2026-09-20 23:27:34 +0530

    V2

--- exception/validationException.js
+++ exception/validationException.js
@@ -3,8 +3,8 @@ export class ValidationError extends Error {}
 export function validateMovie(movie) {
   if (!movie.title) throw new ValidationError("Title is required");
   if (Number.isNaN(movie.releaseYear) ||
-      movie.releaseYear < 1900 || movie.releaseYear > 2100) {
-    throw new ValidationError("Year must be between 1900 and 2100");
+      movie.releaseYear < 1888 || movie.releaseYear > 2026) {
+    throw new ValidationError("Enter a Valid year");
   }
   if (Number.isNaN(movie.rating) || movie.rating < 0 || movie.rating > 10) {
     throw new ValidationError("Rating must be between 0 and 10");
