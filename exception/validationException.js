b2fffbaea5a61b1b11620a11d1b40bbb33605791
CharanUggala <charanuggala@gmail.com>
2026-09-21 12:06:43 +0530

    validation update

--- exception/validationException.js
+++ exception/validationException.js
@@ -1,13 +1,48 @@
-export class ValidationError extends Error {}
-
-export function validateMovie(movie) {
-  if (!movie.title) throw new ValidationError("Title is required");
-  if (Number.isNaN(movie.releaseYear) ||
-      movie.releaseYear < 1888 || movie.releaseYear > 2026) {
-    throw new ValidationError("Enter a Valid year");
+export class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
   }
-  if (Number.isNaN(movie.rating) || movie.rating < 0 || movie.rating > 10) {
+}
+export function validateMovie(movie) {
+  if (!movie.title) {
+    throw new ValidationError("Title is required");
+  }
+
+  if (!movie.genre) {
+    throw new ValidationError("Genre is required");
+  }
+
+  if (!movie.language) {
+    throw new ValidationError("Language is required");
+  }
+
+  if (!movie.releaseYear) {
+    throw new ValidationError("Release year is required");
+  }
+
+  if (Number.isNaN(movie.releaseYear) ||
+      movie.releaseYear < 1888 ||
+      movie.releaseYear > 2026) {
+    throw new ValidationError("Enter a valid year");
+  }
+
+  if (!movie.rating) {
+    throw new ValidationError("Rating is required");
+  }
+
+  if (Number.isNaN(movie.rating) ||
+      movie.rating < 0 ||
+      movie.rating > 10) {
     throw new ValidationError("Rating must be between 0 and 10");
+  }
+
+  if (!movie.duration) {
+    throw new ValidationError("Duration is required");
+  }
+
+  if (!movie.description) {
+    throw new ValidationError("Description is required");
   }
 }
 
